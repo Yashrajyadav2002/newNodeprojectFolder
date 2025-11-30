cb59a6c7502d90b530da7ade79e93a7d2eab1e1c
yashrajyadav2002 <yyash9631@gmail.com>
2025-11-30 23:08:06 +0530

    practice

--- stream part 2/backend/pipe.js
+++ stream part 2/backend/pipe.js
@@ -20,3 +20,28 @@ writeableStream.on('finish',()=>{
 })
 
 
+ //!!!!!!!!!!!!!!!! repeact !!!!!!!!!!!!!!!!!!!
+
+ const fs = require("fs");
+
+// create read and write stream
+
+const readableStream1 = fs.createReadStream('source.txt');
+const writeableStream1 = fs.createWriteStream('destination.txt');
+
+// pipe the readable to writable stream
+
+readableStream.pipe(writeableStream);
+
+// handle complation and errorors
+
+readableStream.on('error',(err)=>{
+    console.error("Read error",err);
+});
+
+writeableStream.on('finish',()=>{
+    console.log('file copy complited');
+})
+
+
+
