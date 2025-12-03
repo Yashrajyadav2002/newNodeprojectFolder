35daebf87b6a4a3ce6de39a2201d3478b248b6a7
yashrajyadav2002 <yyash9631@gmail.com>
2025-12-03 23:35:06 +0530

    realchatapp

---
+++ Real chat app/client/src/App.jsx
@@ -0,0 +1,20 @@
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import Layout from './Layout';
+import Home from './pages/Home';
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+           <Route path="/" element={<Layout></Layout>}>
+             <Route index element={<Home></Home>}></Route>
+           </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
---
+++ Real chat app/server/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const http = require("http");
+const {Server} = require("socket.io");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require('dotenv').config();
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io=new Server(server, {
+    cors: {
+        origin: "http://localhost:5173",
+        methods: ['GET', 'POST']
+    }
+});
+
+io.on('connection', (socket)=>{
+    console.log(`User connected: ${socket.id}`);
+
+    socket.on('send_message', (data)=>{
+        console.log(data);
+        socket.broadcast.emit('receive_message', data);
+    })
+
+    socket.on('disconnect', ()=>{
+        console.log("User disconnected", socket.id);
+    });
+});
+
+server.listen(5000, ()=>{
+    console.log("server running on port 5000");
+})
