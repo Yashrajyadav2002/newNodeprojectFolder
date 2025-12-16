ab6f1bf66d8afd9ba049c3fc27c9699ae6c90209
yashrajyadav2002 <yyash9631@gmail.com>
2025-12-16 23:33:30 +0530

    run

--- New folder/server/index.js
+++ New folder/server/index.js
@@ -1,10 +1,26 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
 const mongoose = require("mongoose");
+const bodyParser = require('body-parser');
+const cors = require("cors");
+const AdminRoute = require("./routes/adminRoute");
+mongoose.connect(process.env.DBCONN).then(()=>{
+    console.log("Database Succesfully Connected!");
+})
 
-mongoose.connect();
+// Use body-parser middleware for JSON and URL-encoded data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+app.use("/admin", AdminRoute);
 
 
-app.listen(3500,()=>{
-    console.log("server is running on port 3500")
+
+
+
+
+app.listen(3500, ()=>{
+    console.log("server run on 8000 Port!")
 })
