e1af449adbdba5508d9c713647c577f01ae00dcf
yashrajyadav2002 <yyash9631@gmail.com>
2025-12-18 23:23:05 +0530

    front

---
+++ New folder/client/Layout.jsx
@@ -0,0 +1,24 @@
+
+
+import { Outlet } from "react-router-dom";
+import Header from "./components/Header";
+import TopNavbar from "./components/TopNavbar";
+import Footer from "./components/Footer";
+
+
+const Layout =()=>{
+    return(
+        <>
+           <Header/>
+           <TopNavbar/>
+           
+           <Outlet/>
+
+           <Footer/>
+
+
+        </>
+    )
+}
+
+export default Layout;
