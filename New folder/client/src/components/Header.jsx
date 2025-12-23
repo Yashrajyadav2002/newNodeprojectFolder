322c5a6899293c12bbffeea8ba534879b258f6fb
yashrajyadav2002 <yyash9631@gmail.com>
2025-12-23 23:30:34 +0530

    navbar

---
+++ New folder/client/src/components/Header.jsx
@@ -0,0 +1,21 @@
+import { FaShoppingCart } from "react-icons/fa";
+
+
+const Header=()=>{
+    return(
+        <>
+          <div id="header">
+            <div></div>
+            <div></div>
+            <div>
+                  <FaShoppingCart />
+            </div>
+          
+
+
+          </div>
+        </>
+    )
+}
+
+export default Header;
